9c6b03b7f1592c18e499c407a96536d013c90d84
lukehayes <lukepurplehayes@gmail.com>
2020-11-18 17:35:19 +0000

    Added $request property to Router class

--- src/Router.php
+++ src/Router.php
@@ -6,6 +6,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Router implements RouterInterface
 {
+    /**
+     * The request object created from Request::createFromGlobals().
+     *
+     * @var Object | NULL
+     */
+    private $request = NULL;
+
+    public function __construct()
+    {
+        $this->request = Request::createFromGlobals();
+    }
+
     public function handle() : void
     {
         // TODO
