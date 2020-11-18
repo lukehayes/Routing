bea14cb9897d9a014a3337b4e51fd4ece405c35f
lukehayes <lukepurplehayes@gmail.com>
2020-11-18 17:25:04 +0000

    Added get and post method stubs

--- src/Router.php
+++ src/Router.php
@@ -2,11 +2,23 @@
 namespace Routing;
 
 use Routing\Interfaces\RouterInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class Router implements RouterInterface
 {
     public function handle() : void
     {
+        // TODO
+    }
+
+    public function get($path)
+    {
+        // TODO
+    }
+
+    public function post($path)
+    {
+        // TODO
     }
 }
 
